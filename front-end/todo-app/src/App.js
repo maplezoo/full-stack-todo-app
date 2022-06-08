4bbc918acc26ba0f68ddbc24d798111d7ed58015
Maple <maple.zhu7@gmail.com>
2022-06-07 21:50:28 -0700

    init todo app jsx

--- front-end/todo-app/src/App.js
+++ front-end/todo-app/src/App.js
@@ -4,9 +4,6 @@ import FirstComp from "./components/first-comp";
 function App() {
   return (
     <div className="App">
-      My Hello World
-        <FirstComp />
-
     </div>
   );
 }
--- front-end/todo-app/src/components/first-comp.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const first =  ()=>{
-    return (<div className="first-component">First Component</div>);
-}
-
-export default first;
